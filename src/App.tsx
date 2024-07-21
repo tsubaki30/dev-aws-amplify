908ad02f6a9a1b8e7a6dd4ee12be9abd31d55783
tsubaki <tsubaki.kentaro@natasha.co.jp>
2024-07-21 20:32:35 +0900

    ユーザー別データ参照追加

--- src/App.tsx
+++ src/App.tsx
@@ -24,21 +24,22 @@ function App() {
 
   return (
       <Authenticator>
-          {({ signOut }) => (
+          {({ signOut, user }) => (
               <main>
+                  <h1>{user?.signInDetails?.loginId}'s todos</h1>
                   <h1>My todos</h1>
                   <button onClick={createTodo}>+ new</button>
                   <ul>
-                      {todos.map((todo) => (
-                          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
-                      ))}
+                    {todos.map((todo) => (
+                        <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
+                    ))}
                   </ul>
                   <div>
-                      🥳 App successfully hosted. Try creating a new todo.
-                      <br/>
-                      <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-                          Review next step of this tutorial.
-                      </a>
+                    🥳 App successfully hosted. Try creating a new todo.
+                    <br/>
+                    <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+                      Review next step of this tutorial.
+                    </a>
                   </div>
                   <button onClick={signOut}>Sign out</button>
               </main>
